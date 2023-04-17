d98d19b5c7475c9881d2016120357cd5c17f9f57
yencal <cyenusah@gmail.com>
2023-04-17 16:20:28 -0600

    some update

--- some_project/main.cpp
+++ some_project/main.cpp
@@ -8,7 +8,9 @@ int main() {
 
     printf("Hello World\n");
 
-    auto test = CArray <int> (5, 5); 
+    auto test = CArray <int> (5, 5);
+
+    auto test1 = CArrayKokkos <int> (5,5);
 
     return 0;
 }
